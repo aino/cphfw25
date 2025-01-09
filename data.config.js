534700b5c31c2d174ba3f398da8113dd664f9059
davidhellsing <david@aino.agency>
2025-01-09 22:24:49 +0100

    img

--- data.config.js
+++ data.config.js
@@ -5,6 +5,8 @@ export const routes = {
     data: async ({ lang }) => {
       return {
         title: `Hello White!`,
+        image:
+          'https://static.bonniernews.se/gcs/bilder/dn-mly/22375e6f-4d29-42f0-8e1d-664bddc41456.jpeg',
       }
     },
   },
